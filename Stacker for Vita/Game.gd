d91217ae96a4ca3fc844a9b9ddd56d9546923982
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-02-24 11:51:05 +0100

    A bit of code cleaning

--- Stacker for Vita/Game.gd
+++ Stacker for Vita/Game.gd
@@ -5,7 +5,8 @@ var rows = 5
 var cols = 3
 
 var gameWindow = [] # To control the blocks logic 
-var line = 0 # number of lines stacked successfully in current run
+var line = 0 # number of lines stacked successfully in current run, reset every few times for ram/screen efficency
+var score = 0 # it's the number of lines completed
 var blocks = 1 # number of currently moving blocks
 var nClean = 8 # max number of lines in screen, avoid getting out of display boundaries
 var refreshTime = 4000000 # Time for the shift time (hacky method I had to come up with)
@@ -21,10 +22,10 @@ func _ready():
 	# if the position has a 0, there's no block in it
 	# if the position has a 1, there's a block in it
 	newRow(blocks)
-	
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	
 	if Input.is_action_just_pressed("Red_Button"):
 		if line != 0: # The first line always stacks
 			# Compare previous and current row to see what blocks that stack and update accordingly
@@ -34,46 +35,34 @@ func _process(_delta):
 				var elem1 = gameWindow.pop_at(prevLineStart + i) # grab and save the elem to compare
 				gameWindow.insert(prevLineStart + i, elem1) # but don't modify the array
 				var elem2 = gameWindow.pop_at(lineStart + i) # grab and save the elem to compare
-				
-				
 				# COMPARE FIRST ELEMENT OF THE PAIR, THE 2 ONE ARE ALWAYS DIFFERENT IMAGE INSTANCES
 				if elem1[0] != elem2[0]: # always empty if they are different
 					if elem1[0] == 1: # If it was a block falling,
 						blocks = blocks - 1 # update the counter
-						
 					# Insert the empty block in the row
 					gameWindow.insert(lineStart + i, elem1)
 				if elem1[0] == elem2[0]: # elem was either empty or stack, so reinsert in same place
 					gameWindow.insert(lineStart + i, elem2)
-
-				print_debug(str(elem1) + " " + str(elem2))
-				print_debug("///")
-				
-				
 			# Check Game Over case
 			if blocks == 0: # There are no ocupied blocks in the row
 				pass # TO-DO
-				
-			# Create the new line so the game can continue
+		
+		# Create the new line so the game can continue
 		line = line + 1
 		newRow(blocks)
-			
 		# Avoid overflowing the screen and save resources
 		# Delete first row of the array when there are too many lines on screen
 		if line > nClean:
 			line = line - 1 
 			for i in range(cols):
 				gameWindow.pop_front() 
-				# TO-DO free the images instanced of that row
-				# TO-DO move displayed blocks one row down
-				
 		
-		
+		# Update current Score
+		score = score + 1
+		get_node("Score").text = "SCORE:\n" + str(score)
 		
 	# Shift current line of blocks moving
 	shiftRow(line)
-	# Display current stack
-	get_node("Score").text = str(gameWindow)
 
 
 # To insert the new line of blocks
@@ -89,21 +78,18 @@ func newRow(size):
 			newOcupy.position = Vector2(line * bCoordinates.x, i * bCoordinates.y)
 			newOcupy.set_visible(true)
 			gameWindow.push_back([1,newOcupy.get_instance_id()])
-			
 		else: # fill the rest of the row with empty spaces
 			var newEmpty = emptySquares.instance()
 			add_child(newEmpty)
 			newEmpty.position = Vector2(line * bCoordinates.x, i * bCoordinates.y)
 			newEmpty.set_visible(true)
 			gameWindow.push_back([0,newEmpty.get_instance_id()])
-			
+
 
 # To Shift the current row (replicates game's movement)
 func shiftRow(row):
 	for i in refreshTime: pass # This is a hacky way to control the refreshing times, so it can be played by a human
-
 	var rowStart = row * cols
-
 	var elem1 = gameWindow.pop_at(rowStart) # current elem in check
 	gameWindow.insert(rowStart, elem1)
 	var elem2 = 0 # To avoid error (wrong element checked) with insertions
@@ -124,4 +110,3 @@ func shiftRow(row):
 # To restart the game
 func _on_NewGame_pressed():
 	Autoload.goto_scene("res://Game.tscn")
-	
