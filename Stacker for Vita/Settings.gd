e6884ac12382b321d1b1c23ab66b8c2d6824dfc5
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-02-26 00:45:56 +0100

    settings without refresh

--- Stacker for Vita/Settings.gd
+++ Stacker for Vita/Settings.gd
@@ -6,16 +6,45 @@ var cols
 var blocks
 var refreshTime
 
+
 func _ready():
+	get_node("OcupiedSpace/Image2").set_visible(true)
 	cols = get_node("/root/Autoload").cols
-	
 	blocks = get_node("/root/Autoload").blocks
 	refreshTime = get_node("/root/Autoload").refreshTime
 
+
 func _on_BlocksUP_pressed():
-	blocks += 1
-	get_node("/root/Autoload").blocks = blocks
-	get_node("RectangleMenu/Blocks").text = "BLOCKS\n" + str(blocks)
+	if blocks < (cols - 1):
+		blocks += 1
+		get_node("/root/Autoload").blocks = blocks
+		get_node("RectangleBlocks/Blocks").text = "BLOCKS\n" + str(blocks)
+
+
+func _on_BlocksDOWN_pressed():
+	if blocks > 1:
+		blocks -= 1
+		get_node("/root/Autoload").blocks = blocks
+		get_node("RectangleBlocks/Blocks").text = "BLOCKS\n" + str(blocks)
+
+
+func _on_ColsUP_pressed():
+	if cols < 7:
+		cols += 1
+		get_node("/root/Autoload").cols = cols
+		get_node("RectangleCols/Cols").text = "COLS\n" + str(cols)
+
+
+func _on_ColsDOWN_pressed():
+	if cols > 2:
+		cols -= 1
+		get_node("/root/Autoload").cols = cols
+		get_node("RectangleCols/Cols").text = "COLS\n" + str(cols)
+# Fix block limits after cols edits, for limit cases
+	if blocks >= cols:
+		blocks = cols -1
+		get_node("/root/Autoload").blocks = blocks
+		get_node("RectangleBlocks/Blocks").text = "BLOCKS\n" + str(blocks)
 
 
 func _on_StartGame_pressed():
